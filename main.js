f65a975e688abdf271f38f9c62f45f162e88b23e
Fr!zz <58346477+frizz2348@users.noreply.github.com>
2022-10-21 11:33:41 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+
+let amount = document.getElementById("amount");
+let nama = document.getElementById("nama");
+let j = document.getElementById("j");
+let namk = document.getElementById("namk");
+
+amount.innerHTML = prompt('What\'s the amount');
+
+j.innerHTML = prompt('What\'s the abbreviation');
+
+nama.innerHTML = prompt('What\'s the name');
+
+namk.innerHTML = prompt('What\'s the name?');
+
+
+
+
+
