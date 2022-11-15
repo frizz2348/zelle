a97b177c852ad1c31fb02af87511e4d703c9f164
Fr!zz <58346477+frizz2348@users.noreply.github.com>
2022-11-14 20:41:08 -0500

    Add files via upload

---
+++ history.js
@@ -0,0 +1,26 @@
+let pend = document.getElementById("pend");
+let abbr1 = document.getElementById("abbr1");
+let name1 = document.getElementById("name1");
+let date1 = document.getElementById("date1");
+let amount1 = document.getElementById("amount1");
+let abbr2 = document.getElementById("abbr2");
+let name2 = document.getElementById("name2");
+let date2 = document.getElementById("date2");
+let amount2 = document.getElementById("amount2");
+
+
+
+
+
+
+
+
+pend.innerHTML = prompt('How many transactions are they?');
+abbr1.innerHTML = prompt('What\'s the abbreviation?');
+name1.innerHTML = prompt('What\'s the name?');
+date1.innerHTML = prompt('What\'s today\'s date?');
+amount1.innerHTML = prompt('What\'s the amount?');
+abbr2.innerHTML = prompt('What\'s the abbreviation?');
+name2.innerHTML = prompt('What\'s the name?');
+date2.innerHTML = prompt('What\'s today\'s date?');
+amount2.innerHTML = prompt('What\'s the amount?');
